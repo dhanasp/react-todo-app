20820453e19efb54da9313553a71e9c0490143c5
Dhanashri Pagar <24383@thoughtworks.com>
2022-07-29 17:17:52 +0530

    Add createTask component

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 
 class CreateTask extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {task: ''}
+    }
+
     render() {
         return (
-            <div>
-                Create Task
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.task} placeholder="Enter Task" onChange={this.handleChange}
+                       autoFocus/>
+                <button type="submit">Add</button>
+            </form>
         );
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.createTaskForTodo(this.state.task);
+        this.setState({task: ''})
+    }
+
+    handleChange = (event) => {
+        this.setState({task: event.target.value})
     }
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CreateTask from "./CreateTask";
 import TaskList from "./TaskList";
 
+const tasks = []; //local storage to fetch previous tasks
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -10,12 +11,22 @@ class Main extends Component {
         }
     }
 
+    createTask = (task) => {
+        if (task.trim === '') {
+            alert("task can\'t be empty")
+            return;
+        }
+
+        tasks.push({task, isCompleted: false})
+        this.setState({tasks: tasks})
+    }
+
     render() {
         return (
             <div>
                 <h1>Todos</h1>
                 <div>
-                    <CreateTask/>
+                    <CreateTask createTaskForTodo={this.createTask}/>
                     <br/>
                     <TaskList/>
                 </div>
